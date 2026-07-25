13c81c91c1cef4acb14648ae8666a2737f52e4b0
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-07-25 23:36:53 +0500

    feat: add remove command

---
+++ src/commands/remove.rs
@@ -0,0 +1,23 @@
+use anyhow::{Ok, Result, bail};
+use std::process::Command;
+
+pub fn execute(packages: Vec<String>) -> Result<()> {
+    if packages.is_empty() {
+        bail!("No package specified.")
+    }
+
+    println!("Installing: {}", packages.join(", "));
+
+    let status = Command::new("sudo")
+        .arg("pacman")
+        .arg("-Rns")
+        .args(&packages)
+        .status()?;
+
+    if status.success() {
+        println!("Installation completed.");
+        Ok(())
+    } else {
+        bail!("pacman exited with status: {}", status);
+    }
+}
