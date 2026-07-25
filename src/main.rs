531bfc06a86f116e702b17d1702cca27a44343c4
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-07-25 23:56:36 +0500

    refactor(cli): dispatch commands to handlers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+mod cli;
+mod commands;
+
+use anyhow::Result;
+use clap::Parser;
+
+use cli::{Cli, Commands};
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Install { packages } => {
+            commands::install::execute(packages)?;
+        }
+
+        Commands::Remove { packages } => {
+            commands::remove::execute(packages)?;
+        }
+
+        Commands::Search { query } => {
+            commands::search::execute(query)?;
+        }
+
+        Commands::Update => {
+            commands::update::execute()?;
+        }
+
+        Commands::Upgrade { packages } => {
+            commands::upgrade::execute(packages)?;
+        }
+    }
+
+    Ok(())
 }
